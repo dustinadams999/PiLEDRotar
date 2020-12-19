03bd7d82ff28343ab8cc7c0323373fc43af8b61b
Dustin Adams <dustinadams01@gmail.com>
2020-12-18 20:14:12 -0600

    Adding rotary and led files to repo.

---
+++ led_fade.py
@@ -0,0 +1,63 @@
+import math
+import pigpio
+from pigpio_encoder import pigpio_encoder
+import time
+
+def main():
+  RED_PIN=5
+  GREEN_PIN=6
+  BLUE_PIN=26
+
+  FACTOR=100
+
+  pi = pigpio.pi()
+
+  r = 0
+  g = 255
+  b = 0
+
+  r_factor = 1
+  g_factor = 3
+  b_factor = 1
+
+  mode = 0
+  degrees = 1
+
+  while 1:
+    time.sleep(.12)
+
+    # at the beginning r = 255, g = 0, b = 0 (b is still, g is fast moving)
+    if mode == 0:
+      r = round((255/2)*(1+math.cos(math.radians(degrees))))
+      g = round((255/2)*(1-math.cos(math.radians(3*degrees))))
+      if degrees%180 == 0:
+        mode = (mode+1)%3
+    # at the end r = 0, g = 255, b = 0
+
+
+    # at the beginning r = 0, g = 255, b = 0 (r is still, b is fast moving)
+    elif mode == 1:
+      b = round((255/2)*(1+math.cos(math.radians(degrees))))
+      g = round((255/2)*(1-math.cos(math.radians(3*degrees))))
+      if degrees%180 == 0:
+        mode = (mode+1)%3
+    # at the end r = 0, g = 0, b = 255
+
+    # at the beginning r = 0, g = 0, b = 255 (g is still, r is fast moving)
+    elif mode == 2:
+      b = round((255/2)*(1+math.cos(math.radians(degrees))))
+      r = round((255/2)*(1-math.cos(math.radians(3*degrees))))
+      if degrees%180 == 0:
+        degrees = 1
+        mode = (mode+1)%3
+    # at the end r = 255, g = 0, b = 0
+
+    pi.set_PWM_dutycycle(RED_PIN, r)
+    pi.set_PWM_dutycycle(GREEN_PIN, g)
+    pi.set_PWM_dutycycle(BLUE_PIN, b)
+
+
+    degrees = (degrees + 1)%360
+
+if __name__ == '__main__':
+  main()
---
+++ rotary_led.py
@@ -0,0 +1,70 @@
+from IPython import embed as shell
+import pigpio
+from pigpio_encoder import pigpio_encoder
+
+RED_PIN=5
+GREEN_PIN=6
+BLUE_PIN=26
+
+rgb = (0,0,0)
+curr_color = 0
+mode = 0
+
+color_keys = ['R', 'G', 'B']
+
+colors = {
+  'R': (lambda rgb,counter: ((bound(0, 255, counter), rgb[1], rgb[2]))),
+  'G': (lambda rgb,counter: ((rgb[0], bound(0, 255, counter), rgb[2]))),
+  'B': (lambda rgb,counter: ((rgb[0], rgb[1], bound(0, 255, counter))))
+}
+
+pi = pigpio.pi()
+
+my_rotary = pigpio_encoder.Rotary(clk=18, dt=17, sw=27)
+
+def main():
+  global my_rotary, curr_color, rgb, pi
+
+  rgb = colors[color_keys[curr_color]](rgb, my_rotary.counter)
+
+  print('starting on color: {}, counter: {}'.format(color_keys[curr_color], my_rotary.counter ))
+
+  my_rotary.setup_rotary(min=0,max=255,scale=8,debounce=50,rotary_callback=rotary_callback)
+  my_rotary.setup_switch(debounce=300,sw_short_callback=sw_short_callback)
+  my_rotary.setup_switch(debounce=100,long_press=sw_long_callback,sw_long_callback=sw_long_callback)
+
+  pi.set_PWM_dutycycle(RED_PIN, rgb[0])
+  pi.set_PWM_dutycycle(GREEN_PIN, rgb[1])
+  pi.set_PWM_dutycycle(BLUE_PIN, rgb[2])
+
+  my_rotary.watch()
+
+def rotary_callback(counter):
+  global rgb, my_rotary, curr_color, pi, offset
+  
+  rgb = colors[color_keys[curr_color]](rgb, my_rotary.counter)
+  
+  pi.set_PWM_dutycycle(RED_PIN, rgb[0])
+  pi.set_PWM_dutycycle(GREEN_PIN, rgb[1])
+  pi.set_PWM_dutycycle(BLUE_PIN, rgb[2])
+
+  print('rgb: {}'.format(rgb))
+
+def sw_short_callback():
+  global my_rotary, rgb, curr_color, offset
+
+  print('click')
+  curr_color = (curr_color + 1)%3
+  my_rotary.counter = rgb[curr_color]
+
+  print('rgb: {}, counter: {}'.format(rgb, my_rotary.counter))
+  print('currently color: {}'.format(color_keys[curr_color]))
+
+def sw_long_callback():
+  print('sw long callback')
+
+def bound(low, high, value):
+  return max(low, min(high, value))
+
+if __name__ == '__main__':
+  main()
