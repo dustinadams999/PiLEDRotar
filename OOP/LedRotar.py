6e472243266db1733e0d942eb86f8363434c9d72
Dustin Adams <dustinadams01@gmail.com>
2020-12-19 09:36:12 -0800

    ready to test on raspberry pi.

--- OOP/LedRotar.py
+++ OOP/LedRotar.py
@@ -7,7 +7,7 @@ curr_color = 0
 mode = 0
 
 pi = None
-my_rotary = None
+led_rotary = None
 
 color_keys = ['R', 'G', 'B']
 
@@ -17,28 +17,29 @@ colors = {
   'B': (lambda rgb,counter: ((rgb[0], rgb[1], bound(0, 255, counter))))
 }
 class LedRotar():
-  def __init__(self, p, my_r):
+  def __init__(self, p, main_rotary):
     pi = p
-    my_rotary = my_r
+    #led_rotary = main_rotary
+    led_rotary = pigpio_encoder.Rotary(clk=18, dt=17, sw=27)
 
-    rgb = colors[color_keys[curr_color]](rgb, my_rotary.counter)
+    rgb = colors[color_keys[curr_color]](rgb, led_rotary.counter)
 
-    print('starting on color: {}, counter: {}'.format(color_keys[curr_color], my_rotary.counter ))
+    print('starting on color: {}, counter: {}'.format(color_keys[curr_color], led_rotary.counter ))
 
-    my_rotary.setup_rotary(min=0,max=255,scale=8,debounce=50,rotary_callback=rotary_callback)
-    my_rotary.setup_switch(debounce=300,sw_short_callback=sw_short_callback)
-    my_rotary.setup_switch(debounce=100,long_press=sw_long_callback,sw_long_callback=sw_long_callback)
+    led_rotary.setup_rotary(min=0,max=255,scale=8,debounce=50,rotary_callback=rotary_callback)
+    led_rotary.setup_switch(debounce=300,sw_short_callback=sw_short_callback)
+    led_rotary.setup_switch(debounce=100,long_press=sw_long_callback,sw_long_callback=sw_long_callback)
 
     pi.set_PWM_dutycycle(RED_PIN, rgb[0])
     pi.set_PWM_dutycycle(GREEN_PIN, rgb[1])
     pi.set_PWM_dutycycle(BLUE_PIN, rgb[2])
 
-    my_rotary.watch()
+    led_rotary.watch()
 
   def rotary_callback(counter):
-    #global rgb, my_rotary, curr_color, pi, offset
+    #global rgb, led_rotary, curr_color, pi, offset
     
-    rgb = colors[color_keys[curr_color]](rgb, my_rotary.counter)
+    rgb = colors[color_keys[curr_color]](rgb, led_rotary.counter)
     
     pi.set_PWM_dutycycle(RED_PIN, rgb[0])
     pi.set_PWM_dutycycle(GREEN_PIN, rgb[1])
@@ -47,13 +48,13 @@ class LedRotar():
     print('rgb: {}'.format(rgb))
 
   def sw_short_callback():
-    #global my_rotary, rgb, curr_color, offset
+    #global led_rotary, rgb, curr_color, offset
 
     print('click')
     curr_color = (curr_color + 1)%3
-    my_rotary.counter = rgb[curr_color]
+    led_rotary.counter = rgb[curr_color]
 
-    print('rgb: {}, counter: {}'.format(rgb, my_rotary.counter))
+    print('rgb: {}, counter: {}'.format(rgb, led_rotary.counter))
     print('currently color: {}'.format(color_keys[curr_color]))
 
   def sw_long_callback():
--- OOP/main_control.py
+++ OOP/main_control.py
@@ -3,22 +3,36 @@ import LedRotar
 import pigpio
 from pigpio_encoder import pigpio_encoder
 
+mode = 0
+
+curr = None
+pi = None
+led_fader = None
+led_rotar = None
+main_rotary = None
+
 def main():
+  global curr, pi, led_fader, led_rotar
 
-
-
-  my_rotary = pigpio_encoder.Rotary(clk=18, dt=17, sw=27)
-  my_rotary.setup_switch(debounce=100,long_press=sw_long_callback,sw_long_callback=sw_long_callback)
+  main_rotary = pigpio_encoder.Rotary(clk=18, dt=17, sw=27)
+  main_rotary.setup_switch(debounce=100,long_press=sw_long_callback,sw_long_callback=sw_long_callback)
   
   pi = pigpio.pi()
-  led_fader = LedFade(pi)
-  led_rotar = LedRotar(pi, my_rotary)
+  #led_fader = LedFade(pi)
+  #led_rotar = LedRotar(pi, main_rotary)
 
+  curr = LedFade.LedFade(pi)
 
-  my_rotary.watch()
+  main_rotary.watch()
 
 def sw_long_callback():
+  global mode, curr
   print('main_control long press')
+  mode = (mode+1)%2
+  if mode == 0:
+    curr = LedFade.LedFade(pi)
+  else:
+    curr = LedRotar.LedRotar(pi, main_rotary)
 
 if __name__ == '__main__':
   main()
