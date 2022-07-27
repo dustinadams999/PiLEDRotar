4112a6be0039010bc1f44e0adbc644c53db4558b
Dustin Adams <dustinadams01@gmail.com>
2022-07-26 21:01:19 -0500

    Renaming rotary files

---
+++ rotary_red.py
@@ -0,0 +1,28 @@
+# rotar 1 controls red
+import os
+import pigpio
+from pigpio_encoder import pigpio_encoder
+
+rotary = pigpio_encoder.Rotary(clk=12, dt=20, sw=16)
+pi = pigpio.pi()
+RED_PIN=5
+
+def main():
+  rotary.setup_rotary(min=0,max=255,scale=8,debounce=300,rotary_callback=rotary_callback)
+  rotary.setup_switch(debounce=300,short_press=sw_short_callback,sw_short_callback=sw_short_callback,long_press=sw_long_callback,sw_long_callback=sw_long_callback)
+
+  rotary.watch()
+
+def rotary_callback(counter):
+  print('red: {}'.format(counter))
+  pi.set_PWM_dutycycle(RED_PIN, min(counter, 255))
+
+def sw_short_callback():
+
+  print('sw short callback')
+
+def sw_long_callback():
+  print('sw long callback')
+
+if __name__ == '__main__':
+  main()
